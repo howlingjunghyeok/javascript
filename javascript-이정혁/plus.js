cafe7a4b210fcbf1f102036781048fa2c7230fdb
howlingjunghyeok <102003120+howlingjunghyeok@users.noreply.github.com>
2022-09-26 11:51:35 +0900

    Add files via upload

---
+++ javascript-이정혁/plus.js
@@ -0,0 +1,20 @@
+function plus(num1,num2){
+    let result;
+    result= num1 + num2;
+    return result
+}
+function minus(num1,num2){
+    let result;
+    result = num1 - num2;
+    return result
+}
+function a(num1,num2){
+    let result;
+    result = num1 * num2;
+    return result
+}
+function b(num1,num2){
+    let result;
+    result = num1 / num2;
+    return result
+}
